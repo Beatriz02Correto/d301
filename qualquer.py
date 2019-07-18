2e575310d8887a986edfda4d48a9cbe9bc931695
Beatriz <bccp02@gmail.com>
2019-07-18 10:24:42 -0300

    finalizar exercicio

--- qualquer.py
+++ qualquer.py
@@ -24,17 +24,55 @@
 #     print(desenho)    
 #     i+=1
 
-#EXERCICIO 3
+# #EXERCICIO 3
+# import random
 
-def embaralhar(lista):
-valor_temporario = none
-indice_aleatorio = none
+# def embaralhar(lista):
+#   for i in range(len(lista) - 1,0,-1) :
+#     indice_aleatorio = random.randint(0, i)
+#     valor_temporario = lista[i]
+#     lista[i] = lista[indice_aleatorio]
+#     lista[indice_aleatorio] = valor_temporario
+#   return lista
 
-  for  i = len(lista) - 1 i != 0 i-=1 {
-    indice_aleatorio = Math.floor(Math.random() * i)
-    valor_temporario = lista[i]
-    lista[i] = lista[indice_aleatorio]
-    lista[indice_aleatorio] = valor_temporario
-  
-  return lista
+# valores = [1, 2, 3, 4, 5,]
+
+# embaralhado = embaralhar(valores)
+# print(embaralhado)
+
+# EXERCICIO 4
+
+# meninos = ["luiz", 'joão', 'fernando', 'bruno']
+# meninas = ['lais','lolo', 'ana']
+# i = 1
+# for meninos in meninas:
+#   for meninas in meninos:
+#     print (f"casalsinho {i}: {meninos} e {meninas}")
+#     i += 1
+
+# # EXERCICIO 5
+# meninos = ["luiz", 'joão', 'fernando', 'bruno']
+# meninas = ['lais','lolo', 'ana']
+# todos = meninas + meninas
+# i = 1
+# j = 0
+# for pessoa1 in meninas:
+#   for k in range(j, len(todos)):
+#     if pessoa1 != todos[k]:
+#       print (f"casalsinho {i}: {pessoa1} e {todos[k]}")
+#       i += 1
+#   j += 1
+
+# EXERCICIO 6
+
+# iguais = [a, a, a, a, a,]
+# diferente = [a, a, a, b, a,]
+
+# def todos-iguais(lista):
+#     for i in range(1, len(lista)):
+#         if lista[i]!= lista[i-1]:
+#             return False
+#     return True
+
+
 
